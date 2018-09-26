e2c28e8172c3ab47447573baa4183971c0adbbe6
Culili <1107954032@qq.com>
2018-09-27 00:57:42 +0800

    完成文件上传功能2018-9-27 00:57:33

--- src/main/java/com/cabbage/controller/FileController.java
+++ src/main/java/com/cabbage/controller/FileController.java
@@ -1,11 +1,21 @@
 package com.cabbage.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @Controller
 public class FileController {
-
+     public final String UPLOADED_FOLDER = "G:\\testfile\\";
     @RequestMapping(value = "/file")
     public String file(){
         return "/file/index";
@@ -14,5 +24,32 @@ public class FileController {
     @RequestMapping(value = "/fileList")
     public String fileList(){
         return "/file/file_list";
+    }
+    @GetMapping("/uploadStatus")
+    public String successfile(){
+        return "/file/successfile";
+    }
+    @PostMapping("/upload")
+    public String singleFileUpload(@RequestParam("file") MultipartFile file,
+                                   RedirectAttributes redirectAttributes) {
+        if (file.isEmpty()) {
+            redirectAttributes.addFlashAttribute("message", "Please select a file to upload");
+            return "redirect:uploadStatus";
+        }
+
+        try {
+            // Get the file and save it somewhere
+            byte[] bytes = file.getBytes();
+            Path path = Paths.get(UPLOADED_FOLDER + file.getOriginalFilename());
+            Files.write(path, bytes);
+
+            redirectAttributes.addFlashAttribute("message",
+                    "You successfully uploaded '" + file.getOriginalFilename() + "'");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return "redirect:/uploadStatus";
     }
 }
